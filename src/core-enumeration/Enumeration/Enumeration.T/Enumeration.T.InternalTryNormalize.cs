edfc3daea590cf4f08f61fffe1c307e28b21392e
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-04-25 23:26:59 +0300

    Force inlining for InternalTryNormalize

--- src/core-enumeration/Enumeration/Enumeration.T/Enumeration.T.InternalTryNormalize.cs
+++ src/core-enumeration/Enumeration/Enumeration.T/Enumeration.T.InternalTryNormalize.cs
@@ -1,9 +1,12 @@
 #nullable enable
 
+using System.Runtime.CompilerServices;
+
 namespace System
 {
     partial class Enumeration<TEnumeration>
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool InternalTryNormalize(TEnumeration actual, out TEnumeration normalized)
             =>
             EnumerationSet.Value.TryGetValue(actual, out normalized);
