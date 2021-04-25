33806c304c97f117a251f095595dc24a195f20ef
Andrei Sergeev <a.sergeev.official@gmail.com>
2021-04-25 21:31:51 +0300

    Add a TODO comment

--- src/core-enumeration/Enumeration/Enumeration.T/Enumeration.T.InternalTryParse.cs
+++ src/core-enumeration/Enumeration/Enumeration.T/Enumeration.T.InternalTryParse.cs
@@ -6,6 +6,8 @@ namespace System
     {
         private static bool InternalTryGet(TEnumeration actual, out TEnumeration result)
         {
+            // TODO: Implement to return a normalized value from the set
+
             if (EnumerationSet.Value.Contains(actual))
             {
                 result = actual;
